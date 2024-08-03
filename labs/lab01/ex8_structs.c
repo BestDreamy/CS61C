6f1454cff4150aa059efee8e21dd267a04ca5c77
aric <3362484152@qq.com>
2024-08-03 10:48:02 +0800

    Finish lab01

--- labs/lab01/ex8_structs.c
+++ labs/lab01/ex8_structs.c
@@ -4,27 +4,27 @@
 // Structs allow you to hold data items of different types in a single variable
 // Struct definitions can be used to declare a struct variable within your program
 // Struct definitions are typically done outside of a function
-struct Student {
+typedef struct Student {
     int id;
     char* name;
-};
+}Student;
 
 int main() {
   // TODO: declare a variable student of type struct Student
   // Note: this struct is stored on the stack
-  ________________ student;
+  Student student;
 
   // TODO: print out the size of a struct Student
   // While this may seem out of place now, it will be useful in the future!
   // Hint: there's an operator that can calculate this for you!
-  printf("Size of a struct Student: %lu bytes\n", ______);
+  printf("Size of a struct Student: %lu bytes\n", sizeof(Student));
 
   // TODO: set student's id field to 5
   // Hint: the dot notation accesses a struct's fields
-  ______.______ = 5;
+  student.id = 5;
 
   // TODO: print out student's id field
-  printf("Student's ID: %d\n", ______);
+  printf("Student's ID: %d\n", student.id);
 
   return 0;
 }
