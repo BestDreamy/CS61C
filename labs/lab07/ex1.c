6263a5e4dbaae34157d75f66adf0ff6f165b74bb
aric <3362484152@qq.com>
2024-08-13 22:31:03 +0800

    Finish lab07

--- labs/lab07/ex1.c
+++ labs/lab07/ex1.c
@@ -51,10 +51,24 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     long long int result = 0; // This is where you should put your final result!
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
+    int *a = (int*)malloc(sizeof(int) * 4);
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        /* YOUR CODE GOES HERE */
+        unsigned int i = 0;
+        for(; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i data = _mm_loadu_si128((__m128i*)(vals + i));
+            __m128i mask = _mm_cmpgt_epi32(data, _127);
 
-        /* Hint: you'll need a tail case. */
+            data = _mm_and_si128(data, mask);
+
+            _mm_storeu_si128((__m128i*)a, data);
+            for(int j = 0; j < 4; j ++) {
+                result += a[j];
+            }
+        }
+
+        for(; i < NUM_ELEMS; i ++) {
+            if(vals[i] > 127) result += vals[i];
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
@@ -69,11 +83,40 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     long long int result = 0;
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
+    int *a = (int*)malloc(sizeof(int) * 4);
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        /* YOUR CODE GOES HERE */
-        /* Copy your sum_simd() implementation here, and unroll it */
+        unsigned int i = 0;
+        for(; i < NUM_ELEMS / 8 * 8; i += 8) {
+            __m128i data0 = _mm_loadu_si128((__m128i*)(vals + i));
+            __m128i data1 = _mm_loadu_si128((__m128i*)(vals + i + 4));
+            // __m128i data2 = _mm_loadu_si128((__m128i*)(vals + i + 8));
+            // __m128i data3 = _mm_loadu_si128((__m128i*)(vals + i + 12));
 
-        /* Hint: you'll need 1 or maybe 2 tail cases here. */
+            __m128i mask0 = _mm_cmpgt_epi32(data0, _127);
+            __m128i mask1 = _mm_cmpgt_epi32(data1, _127);
+            // __m128i mask2 = _mm_cmpgt_epi32(data2, _127);
+            // __m128i mask3 = _mm_cmpgt_epi32(data3, _127);
+
+            data0 = _mm_and_si128(data0, mask0);
+            data1 = _mm_and_si128(data1, mask1);
+            // data2 = _mm_and_si128(data2, mask2);
+            // data3 = _mm_and_si128(data3, mask3);
+
+            __m128i sum = _mm_add_epi32(data0, data1);
+
+            _mm_storeu_si128((__m128i*)a, sum);
+            // _mm_storeu_si128((__m128i*)(a + 4), data1);
+            // _mm_storeu_si128((__m128i*)(a + 8), data2);
+            // _mm_storeu_si128((__m128i*)(a + 12), data3);
+
+            for(int j = 0; j < 4; j ++) {
+                result += a[j];
+            }
+        }
+
+        for(; i < NUM_ELEMS; i ++) {
+            if(vals[i] > 127) result += vals[i];
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
